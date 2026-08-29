3a92eb845c03dbe1d95fe5cadeeb82cb02bcb446
pavanrajputz <pr7676150@gmail.com>
2026-08-29 13:56:34 +0530

    adjusted the input taking scanner class object

--- src/main/java/server/ShellServer.java
+++ src/main/java/server/ShellServer.java
@@ -8,9 +8,10 @@ import java.util.Scanner;
 
 public class ShellServer {
     public static void start(Map<String, Command> commands) {
+
+        Scanner sc = new Scanner(System.in);
         while(true){
             System.out.print("$ ");
-            Scanner sc = new Scanner(System.in);
             String line = sc.nextLine();
             ParsedCommands command = ParsedCommands.fromInput(line);
             CommandHandler.handle(command, commands);
