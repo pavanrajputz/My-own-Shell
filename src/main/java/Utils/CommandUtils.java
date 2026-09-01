58841bff267c0846420442d2105e5efb19be1af6
pavanrajputz <pr7676150@gmail.com>
2026-09-01 23:01:50 +0530

    implementing the utils

---
+++ src/main/java/Utils/CommandUtils.java
@@ -0,0 +1,10 @@
+package Utils;
+
+import parser.ParsedCommands;
+
+public class CommandUtils {
+    private ParsedCommands parsedCommands;
+    public CommandUtils(ParsedCommands parsedCommands) {
+
+    }
+}
--- src/main/java/commands/TypeCommand.java
+++ src/main/java/commands/TypeCommand.java
@@ -1,7 +1,9 @@
 package commands;
 
+import Utils.CommandUtils;
 import parser.ParsedCommands;
 
+import java.util.List;
 import java.util.Map;
 
 public class TypeCommand implements Command {
@@ -19,7 +21,9 @@ public class TypeCommand implements Command {
                 String type = commands.get(arg).type();
                 System.out.println(type);
             }else{
-                System.out.println(arg +": not found");
+                if(!checkInPath(arg)){
+                    System.out.println(arg+": not found");
+                }
             }
         }
     }
@@ -27,5 +31,14 @@ public class TypeCommand implements Command {
     @Override
     public String type() {
         return "type is shell builtin";
+    }
+
+    private boolean checkInPath(String arg){
+        List<String> paths = CommandUtils.checkCommandInPaths(arg);
+        if(!paths.isEmpty()){
+
+            return true;
+        }
+        return false;
     }
 }
