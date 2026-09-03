5900836ea30dcd78ebff29ec71a754104d2aaed9
pavanrajputz <pr7676150@gmail.com>
2026-09-03 22:03:20 +0530

    #IP1 donen with this stage means created support for run a program

--- src/main/java/server/CommandHandler.java
+++ src/main/java/server/CommandHandler.java
@@ -1,8 +1,12 @@
 package server;
 
+import Utils.CommandUtils;
 import commands.Command;
 import parser.ParsedCommands;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 public class CommandHandler {
@@ -10,7 +14,45 @@ public class CommandHandler {
         if(commands.containsKey(command.command)){
             commands.get(command.command).execute(command);
         }else{
-            System.out.println(command.command + ": command not found");
+            List<String> commandLocations = CommandUtils.checkCommandInPaths(command.command);
+            if(commandLocations.isEmpty()){
+                System.out.println(command.command + ": command not found");
+            }else{
+                try{
+                    int statusCode = runExecutable(command, commandLocations);
+                    if(statusCode != 0){
+                        System.out.println(command.command + ": command failed");
+                    }
+                }catch (IOException | InterruptedException e){
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+            }
         }
+    }
+
+    private static int runExecutable(ParsedCommands command,
+                                     List<String> commandLocations)
+    throws IOException, InterruptedException{
+        if(commandLocations.size() > 1){
+            System.out.println("Multiple executables found");
+        }
+        String executable = commandLocations.getFirst();
+        List<String> args = command.args;
+        File executableFile = new File(executable);
+        if(!executableFile.exists() && !executableFile.canExecute()){
+            System.out.println(executableFile+ " is not executable.");
+            return 1;
+        }
+
+        String processName = executableFile.getName();
+        ProcessBuilder processBuilder = new ProcessBuilder(processName);
+        processBuilder.command().addAll(args);
+
+        processBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+        processBuilder.redirectError(ProcessBuilder.Redirect.INHERIT);
+
+        Process process = processBuilder.start();
+        return process.waitFor();
     }
 }
